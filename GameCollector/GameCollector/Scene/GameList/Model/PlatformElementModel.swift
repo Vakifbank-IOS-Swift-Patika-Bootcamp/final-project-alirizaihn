5ad6d4c5e2c072d94b337abc35fd263c9025b25c
Ali Rıza <aliriza.ihn@gmail.com>
2022-12-11 15:39:47 +0300

    -Add gamelist models

---
+++ GameCollector/GameCollector/Scene/GameList/Model/PlatformElementModel.swift
@@ -0,0 +1,65 @@
+//
+//  PlatformElementModel.swift
+//  GameCollector
+//
+//  Created by Ali Rıza İLHAN on 11.12.2022.
+//
+
+import Foundation
+
+// MARK: - PlatformElementModel
+struct PlatformElementModel: Codable {
+    let platform: PlatformPlatform
+    let releasedAt: String
+    let requirementsEn, requirementsRu: JSONNull?
+
+    enum CodingKeys: String, CodingKey {
+        case platform
+        case releasedAt = "released_at"
+        case requirementsEn = "requirements_en"
+        case requirementsRu = "requirements_ru"
+    }
+}
+// MARK: - PlatformPlatform
+struct PlatformPlatform: Codable {
+    let id: Int
+    let name, slug: String
+    let image, yearEnd: JSONNull?
+    let yearStart: Int?
+    let gamesCount: Int
+    let imageBackground: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, name, slug, image
+        case yearEnd = "year_end"
+        case yearStart = "year_start"
+        case gamesCount = "games_count"
+        case imageBackground = "image_background"
+    }
+}
+
+// MARK: - Encode/decode helpers
+class JSONNull: Codable, Hashable {
+
+    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+        return true
+    }
+
+    public var hashValue: Int {
+        return 0
+    }
+
+    public init() {}
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if !container.decodeNil() {
+            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encodeNil()
+    }
+}
